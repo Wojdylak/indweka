92b9c4860a5fff72db652d4d0570fe08c11c9b8b
Piotr Barszczyk <piotr.barszczyk@netteam.pl>
2023-08-06 15:14:21 +0200

    MatrixType remove element

--- src/Form/WeekdayNotificationType.php
+++ src/Form/WeekdayNotificationType.php
@@ -5,48 +5,99 @@ namespace App\Form;
 use App\Entity\TestWeekdayNotification;
 use App\Enum\Notification;
 use App\Enum\Weekdays;
+use Doctrine\Common\Collections\Collection;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EnumType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
+use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
 
 class WeekdayNotificationType extends AbstractType
 {
+    private PropertyAccessorInterface $propertyAccessor;
+
+    public function __construct(PropertyAccessorInterface $propertyAccessor)
+    {
+        $this->propertyAccessor = $propertyAccessor;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add(
-            'checked',
-            CheckboxType::class,
-            [
-                'label' => '123',
-                'required' => false,
-            ]
-        )
-        ->addEventListener(FormEvents::SUBMIT, [$this, 'onSubmit'])
+                'checked',
+                CheckboxType::class,
+                [
+                    'label' => '123',
+                    'required' => false,
+                ]
+            )
+            ->addEventListener(FormEvents::SUBMIT, [$this, 'onSubmit'])
         ;
     }
 
     public function onSubmit(FormEvent $event)
     {
-        $form = $event->getForm();
         $data = $event->getData();
-
-        if (null !== $data) {
-            $data->setNotification($form->getConfig()->getOption('notification'));
-            $data->setWeekday($form->getConfig()->getOption('weekday'));
+        $collection = $event->getForm()->getConfig()->getOption('collection');
+        if (false === $data->isChecked()) {
+            $collection->removeElement($data);
         }
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults([
-            'data_class' => TestWeekdayNotification::class,
-            'weekday' => null,
-            'notification' => null,
-        ]);
+        $resolver
+            ->setDefaults([
+                'data_class' => TestWeekdayNotification::class,
+                'label' => function (Options $options) {
+                    $row = $options['row'];
+                    $column = $options['column'];
+
+                    return $row->name . ' ' . $column->name;
+                },
+                'required' => false,
+                'mapped' => false,
+                'data' => function (Options $options) {
+                    $collection = $options['collection'];
+                    $optionRow = $options['row'];
+                    $rowFieldName = $options['row_field_name'];
+                    $optionColumn = $options['column'];
+                    $columnFieldName = $options['column_field_name'];
+
+                    foreach ($collection as $item) {
+                        $row = $this->propertyAccessor->getValue($item, $rowFieldName);
+                        $column = $this->propertyAccessor->getValue($item, $columnFieldName);
+                        if ($row->isEqual($optionRow) && $column->isEqual($optionColumn)) {
+                            return $item;
+                        }
+                    }
+
+                    $element = new $options['data_class']();
+                    $this->propertyAccessor->setValue($element, $rowFieldName, $optionRow);
+                    $this->propertyAccessor->setValue($element, $columnFieldName, $optionColumn);
+                    $collection->add($element);
+
+                    return $element;
+                }
+            ])
+            ->setRequired([
+                'row',
+                'row_field_name',
+                'column',
+                'column_field_name',
+                'collection',
+            ])
+            ->setAllowedTypes('row', \UnitEnum::class)
+            ->setAllowedTypes('row_field_name', 'string')
+            ->setAllowedTypes('column', \UnitEnum::class)
+            ->setAllowedTypes('column_field_name', 'string')
+            ->setAllowedTypes('collection', Collection::class)
+        ;
     }
 }
